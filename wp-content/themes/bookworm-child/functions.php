4fac2d79fb237ed4ac3919b0329747ceb2d64eb3
nokadatdd <dat.duong@nokasoft.com>
2022-11-01 23:53:54 +0700

    Update style

--- wp-content/themes/bookworm-child/functions.php
+++ wp-content/themes/bookworm-child/functions.php
@@ -8,3 +8,11 @@
 /**
  * Include all your custom code here
  */
+
+add_filter('woocommerce_currency_symbol', 'change_existing_currency_symbol', 10, 2);
+function change_existing_currency_symbol( $currency_symbol, $currency ) {
+//    switch( $currency ) {
+//        case 'AUD': $currency_symbol = 'AUD$'; break;
+//    }
+    return ' VND';
+}
